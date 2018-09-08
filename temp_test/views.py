9db1921d8136a22f7d9367c1e12ddc55c0388f84
Matthew Kelly <mkonabike@gmail.com>
2018-09-08 10:24:52 -0500

    added url for weather, saving Nashville temp and humidity to db with datasets

--- temp_test/views.py
+++ temp_test/views.py
@@ -3,6 +3,7 @@ from .models import Dataset
 import time
 import datetime
 import sqlite3
+import requests
 
 
 def index(request):
@@ -14,7 +15,14 @@ def stats(request):
 
 def run(self):
 	while True:
-		d = Dataset(grow='1',inside_temperature='77.6', inside_humidity='50.3', outside_temperature='89', outside_humidity='55', water_temperature='75.1', water_pH='77.6', image='pic')
+		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=447afc2326760172ae872c6a9c4f3169'
+		city = 'Nashville'
+		r = requests.get(url.format(city)).json()
+		out_temp = r['main']['temp']
+		out_humidity = r['main']['humidity']
+		print(out_temp)
+		print(out_humidity)
+		d = Dataset(grow='1',inside_temperature='77.6', inside_humidity='50.3', outside_temperature=out_temp, outside_humidity=out_humidity, water_temperature='75.1', water_pH='77.6', image='pic')
 		d.save()
 		print(d)
 		time.sleep(5)
