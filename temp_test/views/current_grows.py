dd4b70980c4bfd711037ebe831eb28bab31d348c
Matthew Kelly <mkonabike@gmail.com>
2018-09-12 10:29:20 -0500

    current grows now filtering by end_date

--- temp_test/views/current_grows.py
+++ temp_test/views/current_grows.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from temp_test.models import Grow
+from datetime import datetime
+from django.utils import timezone
+import pytz
 
 def current_grows(request):
+	#day = datetime.date
+	utc = pytz.UTC
+	today = datetime.today()
+	print(today)
+	now = timezone.now()
+	#print(day)
+	current_grows = []
 	grows = Grow.objects.filter(user=request.user.id)
-	template_name = 'temp_test/current_grows.html'
-	return render(request, template_name, {'grows': grows})
+	for grow in grows:
+		if grow.end_date > utc.localize(today):
+			current_grows.append(grow)
+			print(grow.end_date)
+			template_name = 'temp_test/current_grows.html'
+	return render(request, template_name, {'grows': current_grows})
