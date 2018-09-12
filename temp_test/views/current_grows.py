d579c5a332332f1d1285bb7ef2d82b821b433d48
mkonabike <mkonabike@gmail.com>
2018-09-11 21:52:48 -0500

    images saving to static folder in While loop every 5 secs

--- temp_test/views/current_grows.py
+++ temp_test/views/current_grows.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from temp_test.models import Grow
 
+
 def current_grows(request):
 	grows = Grow.objects.filter(user=request.user.id)
 	template_name = 'temp_test/current_grows.html'
