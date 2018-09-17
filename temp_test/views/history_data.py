0bd8c5347bea95b7d31a595287b518b2d6a719fe
Matthew Kelly <mkonabike@gmail.com>
2018-09-17 11:15:29 -0500

    averages working correctly in template and on chart

--- temp_test/views/history_data.py
+++ temp_test/views/history_data.py
@@ -3,10 +3,6 @@ from django.db.models import Avg
 from temp_test.models import Dataset
 
 
-
-
-
-
 def history_data(request,pk):
 	data = Dataset.objects.filter(grow_id=pk).order_by('-id')
 	avg_in_temp = data.aggregate(Avg('inside_temperature'))
@@ -18,27 +14,21 @@ def history_data(request,pk):
 	avg_water_temp = data.aggregate(Avg('water_temperature'))
 	avg_water_pH = data.aggregate(Avg('water_pH'))
 
-	print('LINE 21HD', avg_in_temp)
-	print(avg_in_humidity)
-	print(avg_out_temp)
-	print(avg_out_humidity)
-	print(avg_water_temp)
-	print(avg_water_pH)
-
-	dataset = []
-	dataset.append(avg_in_temp)
-	dataset.append(avg_in_humidity)
-	dataset.append(avg_out_temp)
-	dataset.append(avg_out_humidity)
-	dataset.append(avg_water_temp)
-	dataset.append(avg_water_pH)
-	print(dataset)
-
-
-
+	# inT = []
+	# inH = []
+	# outT = []
+	# outH = []
+	# waterT = []
+	# waterP = []
+	# inT.append(avg_in_temp)
+	# inH.append(avg_in_humidity)
+	# outT.append(avg_out_temp)
+	# outH.append(avg_out_humidity)
+	# waterT.append(avg_water_temp)
+	# waterP.append(avg_water_pH)
 
 	template_name = 'temp_test/history_data.html'
-	return render(request, template_name, {'dataset': dataset})
+	return render(request, template_name, {'avg_in_temp': avg_in_temp, 'avg_in_humidity': avg_in_humidity,'avg_out_temp': avg_out_temp, 'avg_out_humidity': avg_out_humidity, 'avg_water_temp': avg_water_temp, 'avg_water_pH': avg_water_pH})
 
 
 
