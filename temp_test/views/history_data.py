db51865c7ce2fd73df07218a523b424331b0f72d
Matthew Kelly <mkonabike@gmail.com>
2018-09-17 20:55:31 -0500

    plotting all datasets from grow on line graph for history data

--- temp_test/views/history_data.py
+++ temp_test/views/history_data.py
@@ -4,7 +4,7 @@ from temp_test.models import Dataset
 
 
 def history_data(request,pk):
-	data = Dataset.objects.filter(grow_id=pk).order_by('-id')
+	data = Dataset.objects.filter(grow_id=pk)
 	avg_in_temp = data.aggregate(Avg('inside_temperature'))
 	avg_in_humidity = data.aggregate(Avg('inside_humidity'))
 
@@ -14,21 +14,25 @@ def history_data(request,pk):
 	avg_water_temp = data.aggregate(Avg('water_temperature'))
 	avg_water_pH = data.aggregate(Avg('water_pH'))
 
-	# inT = []
-	# inH = []
-	# outT = []
-	# outH = []
-	# waterT = []
-	# waterP = []
-	# inT.append(avg_in_temp)
-	# inH.append(avg_in_humidity)
-	# outT.append(avg_out_temp)
-	# outH.append(avg_out_humidity)
-	# waterT.append(avg_water_temp)
-	# waterP.append(avg_water_pH)
+
+
+
+	in_temp_data = []
+	in_hum_data = []
+	out_temp_data = []
+	out_hum_data = []
+	water_temp_data = []
+	water_ph_data = []
+	for d in data:
+		in_temp_data.append(d.inside_temperature)
+		in_hum_data.append(d.inside_humidity)
+		out_temp_data.append(d.outside_temperature)
+		out_hum_data.append(d.outside_humidity)
+		water_temp_data.append(d.water_temperature)
+		water_ph_data.append(d.water_pH)
 
 	template_name = 'temp_test/history_data.html'
-	return render(request, template_name, {'avg_in_temp': avg_in_temp, 'avg_in_humidity': avg_in_humidity,'avg_out_temp': avg_out_temp, 'avg_out_humidity': avg_out_humidity, 'avg_water_temp': avg_water_temp, 'avg_water_pH': avg_water_pH})
+	return render(request, template_name, {'data': data, 'water_ph_data': water_ph_data, 'water_temp_data': water_temp_data, 'out_hum_data':out_hum_data,'out_temp_data': out_temp_data,'in_hum_data': in_hum_data, 'in_temp_data': in_temp_data, 'avg_in_temp': avg_in_temp, 'avg_in_humidity': avg_in_humidity,'avg_out_temp': avg_out_temp, 'avg_out_humidity': avg_out_humidity, 'avg_water_temp': avg_water_temp, 'avg_water_pH': avg_water_pH})
 
 
 
